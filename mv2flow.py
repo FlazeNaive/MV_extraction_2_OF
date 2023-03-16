259fc77fd09400a4808d23a87c6735184e03d213
qhe <603015257@qq.com>
2023-03-16 15:51:04 +0100

    cleaned up the code, added bash processing

--- mv2flow.py
+++ mv2flow.py
@@ -6,87 +6,54 @@ import glob
 import flowiz as fz
 import matplotlib.pyplot as plt
 
+#========== FUNCTIONS ==========
 
-# FILEID = "1"
+def get_flow_from_mv(mvs_0): 
+    # motion vectors for blocks
+    flow = np.swapaxes([mvs_0[:, 3], mvs_0[:, 4],   # start point (X0, Y0)
+                        mvs_0[:,5] - mvs_0[:,3],    # delta X
+                        mvs_0[:, 6] - mvs_0[:, 4]], # delta Y
+                        0, 1)
+    # print(np.shape(flow))
+    return flow
 
-# FILEPATH = "data-extra/alley_1/out-2023-03-10T14:52:02/motion_vectors/"
-# FRAMEPATH = "data-extra/alley_1/out-2023-03-10T14:52:02/frames/"
+def vis_flow(flow):
+    vis_mat = np.zeros((h, w, 2))
 
-FILEPATH = "data-extra/"
+    num_vec = np.shape(flow)[0]
+    for mv in np.split(flow, num_vec):
+        # print("---")
+        start_pt = (mv[0][0], mv[0][1])
+        end_pt = (mv[0][2] + start_pt[0], mv[0][3] + start_pt[1]) 
 
-frame_0 = cv2.imread(FRAMEPATH + "frame-" + FILEID + ".jpg")
-print(np.shape(frame_0))
-(h, w, _) = np.shape(frame_0)
-
-# draw_img = np.zeros((w, h))
-draw_img = np.zeros(np.shape(frame_0))
-
-print("draw")
-print(np.shape(draw_img))
-
-mvs_0 = np.load(FILEPATH + "/mvs-" + FILEID + ".npy")
-# print(mvs_0)
-print(mvs_0[0])
-print(mvs_0[-1])
-
-# motion vectors for blocks
-flow = np.swapaxes([mvs_0[:, 3], mvs_0[:, 4],   # start point (X0, Y0)
-                    mvs_0[:,5] - mvs_0[:,3],    # delta X
-                    mvs_0[:, 6] - mvs_0[:, 4]], # delta Y
-                    0, 1)
-# flow = np.ndarray([mvs_0[:,5] - mvs_0[:,3], mvs_0[:, 6] - mvs_0[:, 4]])
-print(np.shape(flow))
-
-vis_mat = np.zeros((h, w, 2))
-
-num_vec = np.shape(flow)[0]
-for mv in np.split(flow, num_vec):
-    # print("---")
-    start_pt = (mv[0][0], mv[0][1])
-    end_pt = (mv[0][2] + start_pt[0], mv[0][3] + start_pt[1]) 
-    # if start_pt[1]-8 > 436 or start_pt[0]-8> 1024:
-    #     print("START:")
-    #     print(start_pt)
-    # if end_pt[1]-8 > 436 or end_pt[0]-8 > 1024:
-    #     print("END:")
-    #     print(end_pt)
-
-    #draw motion vector blocks
-    for i in range(16):
-        for j in range(16):
-            try:
-                cur = (end_pt[1] + i - 8, end_pt[0] + j - 8) # pixel currently on
-                # cur = (start_pt[1] + i - 24, start_pt[0] + j - 16)
-                # draw_img[start_pt[1] + i - 16][start_pt[0] + j - 16] = (mv[0][2], mv[0][2], mv[0][3])
-                # draw_img[end_pt[1] + i - 24][end_pt[0] + j - 8] = (mv[0][2], mv[0][2], mv[0][3])
-                draw_img[cur[0]][cur[1]] = (mv[0][2], mv[0][2], mv[0][3])
-                vis_mat[cur[0]][cur[1]] = (mv[0][2], mv[0][3])
-                # draw_img[cur[0]][cur[1]] *= 20
-                # draw_img[end_pt[1] + i - 16][end_pt[0] + j - 16] = (mv[0][2], mv[0][2], mv[0][3])
-            except:
-                # print("OUT OF RANGE")
-                # occurs because the size is not dividable by 16
-                # print(cur)
-                continue
-                # norm_image = cv2.normalize(draw_img, None, alpha=0, beta=1, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_32F)
-                # cv2.imwrite("flow.jpg", norm_image)
-                # k = input()
-
-# draw_img= cv2.normalize(draw_img, None, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_16SC1)
+        #draw motion vector blocks
+        for i in range(16):
+            for j in range(16):
+                try:
+                    cur = (end_pt[1] + i - 8, end_pt[0] + j - 8) # pixel currently on
+                    vis_mat[cur[0]][cur[1]] = (mv[0][2], mv[0][3])
+                except:
+                    continue
+    return vis_mat
 
 
 # ================ draw vectors ================
-# for mv in np.split(flow, num_vec):
-#     # print("---")
-#     start_pt = (mv[0][0], mv[0][1])
-#     end_pt = (mv[0][2] + start_pt[0], mv[0][3] + start_pt[1]) 
-#     cv2.arrowedLine(draw_img, start_pt, end_pt, (255, 0, 0), 1, cv2.LINE_AA, 0, 0.1)
-
-
-cv2.imwrite("flow.bmp", draw_img)
+def draw_vector(flow, draw_img, OUTPUTFILE):
+    for mv in np.split(flow, num_vec):
+        # print("---")
+        start_pt = (mv[0][0], mv[0][1])
+        end_pt = (mv[0][2] + start_pt[0], mv[0][3] + start_pt[1]) 
+        cv2.arrowedLine(draw_img, start_pt, end_pt, (255, 0, 0), 1, cv2.LINE_AA, 0, 0.1)
+    
+    cv2.imwrite(OUTPUTFILE, draw_img)
 
 # ================ save *.flo ================
 def save_to_flo(vis_mat, filename): 
+    # mkdir if not exist
+    filepath = os.path.dirname(filename)
+    if not os.path.exists(filepath):
+        os.makedirs(filepath)
+    
     height, width, nBands = vis_mat.shape
     assert nBands == 2, "Number of bands = %r != 2" % nBands
     u = vis_mat[: , : , 0]
@@ -106,9 +73,6 @@ def save_to_flo(vis_mat, filename):
 
     f.close()
 
-# filename = "alley_" + FILEID + ".flo"
-save_to_flo(vis_mat, "alley_" + FILEID + ".flo")
-
 # ================ read *.flo ================
 def read_flo_file(filename, memcached=False):
     """
@@ -116,6 +80,7 @@ def read_flo_file(filename, memcached=False):
     :param flow_file: name of the flow file
     :return: optical flow data in matrix
     """
+
     if memcached:
         filename = io.BytesIO(filename)
     f = open(filename, 'rb')
@@ -133,7 +98,102 @@ def read_flo_file(filename, memcached=False):
     f.close()
     return data2d
 
-gt_flow = read_flo_file('frame_0001.flo')
+# ================ save visualized flow ================
+
+def save_flo_to_visual(input_filename, output_filename):
+    # mkdir if not exist
+    filepath = os.path.dirname(output_filename)
+    if not os.path.exists(filepath):
+        os.makedirs(filepath)
+
+    files = glob.glob(input_filename)
+    img = fz.convert_from_file(files[0])
+    plt.imshow(img)
+
+    plt.savefig(output_filename)
+
+
+# ========== MAIN ==========
+
+# FILEPATH = "data-extra/alley_1/out-2023-03-10T14:52:02/motion_vectors/"
+FRAMEPATH = "data-extra/alley_1/out-2023-03-10T14:52:02/frames/"
+
+# get height and width of frame
+frame_0 = cv2.imread(FRAMEPATH + "frame-0.jpg")
+print(np.shape(frame_0))
+(h, w, _) = np.shape(frame_0)
+
+INPUT_BASE = "data-extra/"
+OUTPUT_BASE = "data-output/"
+GT_BASE = "data-gtflow/"
+
+for FILE in os.listdir(INPUT_BASE):
+    PATH1 = INPUT_BASE + FILE + "/"
+    for random_name in os.listdir(PATH1):
+        PATH2 = PATH1 + random_name + "/"
+        print(PATH2)
+
+        MVPATH = PATH2 + "motion_vectors/"
+        TYPES = PATH2 + "frame_types.txt"
+
+        with open(TYPES, "r") as f:
+            # read lines until the end of the file
+            frame_counter = 0
+            while True:
+                frame_type = f.readline()
+
+                if not frame_type:
+                    break
+                frame_type = frame_type.strip()
+
+                #process the P frame, convert to flow
+                if frame_type == "P":
+                    # read the motion vectors
+                    mv_file = MVPATH + "mvs-" + str(frame_counter) + ".npy"
+                    mvs = np.load(mv_file)
+
+                    #get flow from motion vectors
+                    flow = get_flow_from_mv(mvs)
+
+                    # draw the flow matrix
+                    vis_mat = vis_flow(flow)
+
+                    #save the flow matrix
+                    save_to_flo(vis_mat, OUTPUT_BASE + "mv-flo/" + 
+                                    FILE + "/flow-" + str(frame_counter) + ".flo")
+
+                    # convert int to 4-number string using formatted string
+                    print("Current frame #: ", frame_counter)
+                    frame_counter_str = f"{frame_counter:04d}"
+                    print(frame_counter_str)
+
+                    #read ground truth flow
+                    gt_flow = read_flo_file(GT_BASE + FILE + "/frame_" + frame_counter_str + ".flo")
+
+                    #combine ground truth flow and predicted flow
+                    combined_flow = np.concatenate((gt_flow, vis_mat), axis=1)
+
+                    #save the combined flow
+                    save_to_flo(combined_flow, OUTPUT_BASE + "combined-flo/" + 
+                                FILE + "/combined-" + str(frame_counter) + ".flo")
+                    
+                    #save the visualized flow
+                    save_flo_to_visual(OUTPUT_BASE + "combined-flo/" + 
+                                FILE + "/combined-" + str(frame_counter) + ".flo",
+                                OUTPUT_BASE + "vis-flo/" + 
+                                FILE + "/vis-" + str(frame_counter) + ".png"
+                                )
+
+                frame_counter = frame_counter + 1
+        quit()
+                    
+
+
+quit()
+
+
+# print("draw")
+# print(np.shape(draw_img))
 
 # ================ combine ================
 showww = np.concatenate((vis_mat , gt_flow), axis=0)
